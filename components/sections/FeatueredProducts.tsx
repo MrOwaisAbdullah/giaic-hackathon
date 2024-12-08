06051edbb1f8604895d25a8ed7583bf48072bd92
MrOwaisAbdullah <mrowaisabdullah@gmail.com>
2024-12-09 00:58:19 +0500

    Updated

--- components/sections/FeatueredProducts.tsx
+++ components/sections/FeatueredProducts.tsx
@@ -5,7 +5,7 @@ import { inter } from '@/app/fonts'
 const FeatueredProducts = () => {
   return (
     <section className={`${inter.className} xl:px-5 pt-16`}>
-    <h2 className='heading text-center px-2 xl:text-left xl:px-28'>Featured Products</h2>
+    <h2 className='heading text-center px-2 xl:text-left xl:px-20'>Featured Products</h2>
     <div className='container flex flex-wrap xl:flex-nowrap justify-center items-center gap-5'>
         <ProductCard image='/product1.png' newPrice={20} name='Library Stool Chair' isNew={true}  />
         <ProductCard image='/product7.png' newPrice={20} name='Library Stool Chair' isDiscount={true}/>
--- components/sections/Products.tsx
+++ components/sections/Products.tsx
@@ -4,19 +4,20 @@ import { inter } from '@/app/fonts'
 
 const Products = () => {
   return (
-    <section className={`${inter.className} xl:px-5 pt-16`}>
-    <h2 className='heading text-center px-2 xl:text-left xl:px-28'>Our Products</h2>
-    <div className='container flex flex-wrap xl:flex-nowrap justify-center items-center gap-5'>
+    <section className={`${inter.className} xl:px-5 pt-28`}>
+    <h2 className='heading text-center px-2 xl:px-28'>Our Products</h2>
+    <div className='flex flex-wrap justify-center items-center gap-5'>
         <ProductCard image='/product1.png' newPrice={20} name='Library Stool Chair' isNew={true}  />
         <ProductCard image='/product7.png' newPrice={20} name='Library Stool Chair' isDiscount={true}/>
         <ProductCard image='/product6.png' newPrice={20} name='Library Stool Chair' />
         <ProductCard image='/product5.png' newPrice={20} name='Library Stool Chair' />
-    </div>
-    <div className='container flex flex-wrap xl:flex-nowrap justify-center items-center mt-10 gap-5'>
         <ProductCard image='/product4.png' newPrice={20} name='Library Stool Chair' isNew={true}  />
         <ProductCard image='/product3.png' newPrice={20} name='Library Stool Chair' isDiscount={true}/>
         <ProductCard image='/product2.png' newPrice={20} name='Library Stool Chair' />
         <ProductCard image='/product1.png' newPrice={20} name='Library Stool Chair' />
+    </div>
+    <div className='container flex flex-wrap xl:flex-nowrap justify-center items-center mt-10 gap-5'>
+
     </div>
     </section>
 )
