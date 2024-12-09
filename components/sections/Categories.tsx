a01da7ff0fa68bb3fd48edb9a77c5675f3c3f02c
MrOwaisAbdullah <mrowaisabdullah@gmail.com>
2024-12-09 17:01:49 +0500

    Images Fixed

--- components/sections/Categories.tsx
+++ components/sections/Categories.tsx
@@ -1,18 +1,18 @@
 import { inter } from "@/app/fonts";
+import Image from "next/image";
 import React from "react";
+import product4 from "../../public/product4.png"
+import category from "../../public/category.png"
+import category1 from "../../public/category1.png"
 
 const Categories = () => {
   return (
     <section className={`${inter.className} px-5 pt-16`}>
       <h2 className="heading text-center xl:text-left xl:px-28">Top categories</h2>
       <div className="flex flex-wrap xl:flex-nowrap justify-center items-center gap-5">
-        <div
-          className="relative bg-slate-200 rounded-[10px] bg-[url('/product4.png')] h-[350px] w-[350px] bg-cover bg-center"
-          style={{
-            backgroundSize: "100% 140%",
-            backgroundPosition: "right bottom",
-          }}
-        >
+      <div
+          className="relative bg-slate-200 rounded-[10px] h-[350px] w-[350px] overflow-hidden">
+            <Image src={product4} alt='product'/>
           <div className="absolute w-full bottom-0 left-0 rounded-b-[10px] text-white flex flex-col text-left p-2 px-4 justify-center bg-black/60">
             <h3 className="text-xl">Wing Chair</h3>
             <p className="text-sm text-secondary">3,584 Products</p>
@@ -20,12 +20,8 @@ const Categories = () => {
         </div>
 
         <div
-          className="relative bg-slate-200 rounded-[10px] bg-[url('/category.png')] h-[350px] w-[350px] bg-cover bg-center"
-          style={{
-            backgroundSize: "130% 150%",
-            backgroundPosition: "right bottom",
-          }}
-        >
+          className="relative bg-slate-200 rounded-[10px] h-[350px] w-[350px] overflow-hidden">
+            <Image src={category} alt='product'/>
           <div className="absolute w-full bottom-0 left-0 rounded-b-[10px] text-white flex flex-col text-left p-2 px-4 justify-center bg-black/60">
             <h3 className="text-xl">Wing Chair</h3>
             <p className="text-sm text-secondary">3,584 Products</p>
@@ -33,12 +29,8 @@ const Categories = () => {
         </div>
 
         <div
-          className="relative bg-slate-200 rounded-[10px] bg-[url('/category1.png')] h-[350px] w-[350px] bg-cover bg-center"
-          style={{
-            backgroundSize: "160% 110%",
-            backgroundPosition: "right bottom",
-          }}
-        >
+          className="relative bg-slate-200 rounded-[10px] h-[350px] w-[350px] overflow-hidden">
+            <Image src={category1} alt='product'/>
           <div className="absolute w-full bottom-0 left-0 rounded-b-[10px] text-white flex flex-col text-left p-2 px-4 justify-center bg-black/60">
             <h3 className="text-xl">Wing Chair</h3>
             <p className="text-sm text-secondary">3,584 Products</p>
--- components/sections/Gallery.tsx
+++ components/sections/Gallery.tsx
@@ -1,5 +1,11 @@
 import { roboto } from '@/app/fonts'
 import React from 'react'
+import product1 from "../../public/product1.png"
+import product3 from "../../public/product3.png"
+import product5 from "../../public/product5.png"
+import product6 from "../../public/product6.png"
+import product7 from "../../public/product7.png"
+import Image from 'next/image'
 
 const Gallery = () => {
   return (
@@ -11,22 +17,14 @@ const Gallery = () => {
         EXPLORE NEW AND POPULAR STYLES
       </div>
                 <div
-          className="group m-0 relative w-full xl:w-[59%] h-auto mr-5 bg-slate-200 bg-[url('/product1.png')] bg-cover bg-center overflow-hidden"
-          style={{
-            backgroundSize: "100% 100%",
-            backgroundPosition: "right bottom",
-          }}
-        >
+          className="group m-0 relative w-full xl:w-[59%] h-auto mr-5 bg-slate-200 overflow-hidden">
+            <Image src={product6} alt='Product'></Image>
         </div>
 
         <div className='flex gap-5 flex-wrap w-full items-center xl:w-3/5 jutify-center xl:justify-end'>
         <div
-          className="group relative justify-center items-center w-48 h-48 xl:w-56 xl:h-56 bg-slate-200 bg-[url('/product1.png')] bg-cover bg-center overflow-hidden"
-          style={{
-            backgroundSize: "100% 100%",
-            backgroundPosition: "right bottom",
-          }}
-        >
+          className="group relative justify-center items-center w-48 h-48 xl:w-56 xl:h-56 bg-slate-200 bg-[url('/product1.png')] bg-cover bg-center overflow-hidden">
+             <Image src={product5} alt='Product'></Image>
           <div className="absolute w-full -bottom-96 group-hover:bottom-0 left-0 text-white flex flex-col text-left p-2 px-4 items-center justify-between bg-black/80">
             <h3 className="text-xl">Wing Chair</h3>
             <p className="text-sm text-secondary">3,584 Products</p>
@@ -34,12 +32,8 @@ const Gallery = () => {
         </div>
 
         <div
-          className="group relative w-48 h-48 xl:w-56 xl:h-56 bg-slate-200 bg-[url('/product1.png')] bg-cover bg-center overflow-hidden"
-          style={{
-            backgroundSize: "100% 100%",
-            backgroundPosition: "right bottom",
-          }}
-        >
+          className="group relative justify-center items-center w-48 h-48 xl:w-56 xl:h-56 bg-slate-200 bg-[url('/product1.png')] bg-cover bg-center overflow-hidden">
+             <Image src={product3} alt='Product'></Image>
           <div className="absolute w-full -bottom-96 group-hover:bottom-0 left-0 text-white flex flex-col text-left p-2 px-4 items-center justify-between bg-black/80">
             <h3 className="text-xl">Wing Chair</h3>
             <p className="text-sm text-secondary">3,584 Products</p>
@@ -47,12 +41,8 @@ const Gallery = () => {
         </div>
 
         <div
-          className="group relative w-48 h-48 xl:w-56 xl:h-56 bg-slate-200 bg-[url('/product1.png')] bg-cover bg-center overflow-hidden"
-          style={{
-            backgroundSize: "100% 100%",
-            backgroundPosition: "right bottom",
-          }}
-        >
+          className="group relative justify-center items-center w-48 h-48 xl:w-56 xl:h-56 bg-slate-200 bg-[url('/product1.png')] bg-cover bg-center overflow-hidden">
+             <Image src={product1} alt='Product'></Image>
           <div className="absolute w-full -bottom-96 group-hover:bottom-0 left-0 text-white flex flex-col text-left p-2 px-4 items-center justify-between bg-black/80">
             <h3 className="text-xl">Wing Chair</h3>
             <p className="text-sm text-secondary">3,584 Products</p>
@@ -60,12 +50,8 @@ const Gallery = () => {
         </div>
 
         <div
-          className="group relative w-48 h-48 xl:w-56 xl:h-56 bg-slate-200 bg-[url('/product1.png')] bg-cover bg-center overflow-hidden"
-          style={{
-            backgroundSize: "100% 100%",
-            backgroundPosition: "right bottom",
-          }}
-        >
+          className="group relative justify-center items-center w-48 h-48 xl:w-56 xl:h-56 bg-slate-200 bg-[url('/product1.png')] bg-cover bg-center overflow-hidden">
+             <Image src={product7} alt='Product'></Image>
           <div className="absolute w-full -bottom-96 group-hover:bottom-0 left-0 text-white flex flex-col text-left p-2 px-4 items-center justify-between bg-black/80">
             <h3 className="text-xl">Wing Chair</h3>
             <p className="text-sm text-secondary">3,584 Products</p>
