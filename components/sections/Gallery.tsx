8271339472c679db92451a6c3b54898ce5d63ba4
MrOwaisAbdullah <mrowaisabdullah@gmail.com>
2025-01-07 19:59:21 +0500

    Updated

--- components/sections/Gallery.tsx
+++ components/sections/Gallery.tsx
@@ -9,7 +9,7 @@ import Image from 'next/image'
 
 const Gallery = () => {
   return (
-    <section className='grid grid-cols-1 gap-5 xl:gap-0 md:grid-cols-[auto,auto] px-5 xl:px-0 m-auto mt-52 w-full'>
+    <section className='relative grid grid-cols-1 gap-5 xl:gap-0 md:grid-cols-[auto,auto] px-5 xl:px-0 m-auto mt-52 w-full overflow-hidden'>
       {/* vertical Text */}
       <div
         className={`${roboto.className} md:hidden block text-2xl font-medium text-center text-black`}
@@ -30,10 +30,10 @@ const Gallery = () => {
         </div>
         </div>
       {/* four small images */}
-        <div className='grid grid-cols-1 sm:grid-cols-[auto,auto] gap-7 md:max-h-[400px] lg:max-h-[500px] xl:max-h-[550px] w-full'>
+        <div className='grid grid-cols-1 sm:grid-cols-[auto,auto] gap-5 xl:gap-7 w-full'>
         <div
-          className="group relative bg-slate-200 overflow-hidden">
-             <Image src={product5} alt='Product'></Image>
+          className="group relative md:max-h-48 lg:max-h-60 xl:max-h-full bg-slate-200 overflow-hidden">
+             <Image className="object-cover" src={product5} alt='Product'></Image>
           <div className="absolute w-full -bottom-96 group-hover:bottom-0 left-0 text-white flex flex-col text-left p-2 px-4 items-center justify-between bg-black/80">
             <h3 className="text-xl">Wing Chair</h3>
             <p className="text-sm text-secondary">3,584 Products</p>
@@ -41,8 +41,8 @@ const Gallery = () => {
         </div>
 
         <div
-          className="group relative bg-slate-200 overflow-hidden">
-             <Image src={product3} alt='Product'></Image>
+          className="group relative md:max-h-48 lg:max-h-60 xl:max-h-full bg-slate-200 overflow-hidden">
+             <Image className="object-cover" src={product3} alt='Product'></Image>
           <div className="absolute w-full -bottom-96 group-hover:bottom-0 left-0 text-white flex flex-col text-left p-2 px-4 items-center justify-between bg-black/80">
             <h3 className="text-xl">Wing Chair</h3>
             <p className="text-sm text-secondary">3,584 Products</p>
@@ -50,8 +50,8 @@ const Gallery = () => {
         </div>
 
         <div
-          className="group relative bg-slate-200 overflow-hidden">
-             <Image src={product1} alt='Product'></Image>
+          className="group relative md:max-h-48 lg:max-h-60 xl:max-h-full bg-slate-200 overflow-hidden">
+             <Image className="object-cover" src={product1} alt='Product'></Image>
           <div className="absolute w-full -bottom-96 group-hover:bottom-0 left-0 text-white flex flex-col text-left p-2 px-4 items-center justify-between bg-black/80">
             <h3 className="text-xl">Wing Chair</h3>
             <p className="text-sm text-secondary">3,584 Products</p>
@@ -59,8 +59,8 @@ const Gallery = () => {
         </div>
 
         <div
-          className="group relative justify-center items-center bg-slate-200 overflow-hidden">
-             <Image src={product7} alt='Product'></Image>
+          className="group relative md:max-h-48 lg:max-h-60 xl:max-h-full justify-center items-center bg-slate-200 overflow-hidden">
+             <Image className="object-cover" src={product7} alt='Product'></Image>
           <div className="absolute w-full -bottom-96 group-hover:bottom-0 left-0 text-white flex flex-col text-left p-2 px-4 items-center justify-between bg-black/80">
             <h3 className="text-xl">Wing Chair</h3>
             <p className="text-sm text-secondary">3,584 Products</p>
